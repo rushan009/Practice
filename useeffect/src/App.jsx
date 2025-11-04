5b24ed699a003c2c5a853bbc5b6c20e2bcf7b423
Rushan <rushan@Rushans-MacBook-Air.local>
2025-11-04 10:57:41 +0545

    second commit

---
+++ useeffect/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+const App = () => {
+  useEffect(function () {
+    console.log('use effect is running')
+  })
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
