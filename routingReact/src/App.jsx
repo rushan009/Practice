013dc69ba800cefed928567abcfcae24e1b0c545
Rushan <rushan@Rushans-MacBook-Air.local>
2025-11-11 10:42:44 +0545

    forth commit

--- routingReact/src/App.jsx
+++ routingReact/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
 
     </div>
