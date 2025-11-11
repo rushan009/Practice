77d9369089d71c5bb1f74c1d9aabd857c5593fe6
Rushan <rushan@Rushans-MacBook-Air.local>
2025-11-11 10:42:06 +0545

    third commit

--- routingReact/src/App.jsx
+++ routingReact/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+
+    </div>
   )
 }
 
